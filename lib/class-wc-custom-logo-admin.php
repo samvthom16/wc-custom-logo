31e6f8abafda5b515cb5789b2517cbaccbf5060b
Samuel Thomas <samvthom16@gmail.com>
2022-01-27 17:25:56 +0530

    Customise link within variations in single product

--- lib/class-wc-custom-logo-admin.php
+++ lib/class-wc-custom-logo-admin.php
@@ -5,29 +5,20 @@
     function __construct(){
       add_action( 'admin_enqueue_scripts', array( $this, 'loadAssets' ) );
 
-      /*
-      * ADD ANOTHER TAB TO THE PRODUCT DATA SECTION
-      */
-      add_filter( 'woocommerce_product_data_tabs', function( $tabs ){
-        $tabs['wc_custom_logo'] = array(
-      		'label'    => 'Customize',
-      		'target'   => 'wc_custom_logo_container',
-      		'class'    => array(),
-      		'priority' => 21,
-      	);
-      	return $tabs;
+      add_action( 'admin_footer', function(){
+        if( is_admin( 'post.php') ){
+          global $post;
+          $dimensions = get_post_meta( $post->ID, 'wc_logo_dimensions', true );
+          include( 'templates/inject_script.php' );
+        }
       } );
 
-      /*
-      * HTML FOR THE PRODUCT PANELS
-      */
-      //add_action( 'woocommerce_product_data_panels', array( $this, 'productDataPanelsHTML' ) );
-      add_filter( "attachment_fields_to_edit", array( $this, 'addScriptToAttachmentPage' ), null, 2);
+      add_filter("attachment_fields_to_edit", array( $this, "addToAttachmentFields" ), 1, 2);
+      add_action( 'woocommerce_product_after_variable_attributes', array( $this, "addToVariableMetabox"), 10, 3 );
 
       /*
       * SAVE PRODUCT PANEL DATA
       */
-      //add_action('woocommerce_process_product_meta', array( $this, 'saveProductDataPanels') );
       add_action( 'attachment_fields_to_save', array( $this, 'saveProductDataPanels') );
 
     }
@@ -37,19 +28,9 @@
       //wp_enqueue_script( 'wc-logo-draggable', plugins_url( 'assets/js/draggable.min.js' , dirname(__FILE__) ), array( 'jquery', 'jqueryui' ), WC_CUSTOM_LOGO_VERSION );
       wp_enqueue_script( 'wc-logo-admin', plugins_url( 'assets/js/admin.js' , dirname(__FILE__) ), array( 'jquery' ), WC_CUSTOM_LOGO_VERSION );
       wp_localize_script( 'wc-logo-admin', 'wc_defaults', array( 'logo' =>   plugins_url( 'assets/images/custom-logo-placeholder.png' , dirname(__FILE__) ) ) );
-
       wp_enqueue_style( 'wc-logo-admin', plugins_url( 'assets/css/admin.css' , dirname(__FILE__) ), array(), WC_CUSTOM_LOGO_VERSION );
-
-
     }
 
-    /*
-    function productDataPanelsHTML(){
-      $post_id = get_the_ID();
-      $dimensions = wc_get_dimensions( $post_id );
-      $custom_logo_placeholder_src = plugins_url( 'assets/images/custom-logo-placeholder.png' , dirname(__FILE__) );
-      include "templates/product-data-panels.php";
-    }
 
     /*
     * SAVE EACH DIMENSION TO THE PRODUCT META ITSELF
@@ -61,11 +42,37 @@
       }
     }
 
-    function addScriptToAttachmentPage( $form_fields, $post ){
-      $dimensions = get_post_meta( $post->ID, 'wc_logo_dimensions', true );
-      include( 'templates/inject_script.php' );
+    function getEditAttachmentLink( $post_id ){
+      return admin_url() . "post.php?post=" . $post_id . "&action=edit";
+    }
+
+    function getCustomiseBtnHTML( $post_id ){
+      $link = $this->getEditAttachmentLink( $post_id );
+      return '<a class="button button-html" href="'.$link.'" target="_blank">Customise (Add Logo)</a>';
+    }
+
+    /*
+    * THIS METABOX APPEARS INSIDE ATTACHMENT FIELDS WHEN THE IMAGE IS SELECTED THROUGH THE POP UP
+    */
+    function addToAttachmentFields( $form_fields, $post ){
+      if( substr($post->post_mime_type, 0, 5) == 'image' ){
+        $form_fields["wc_custom_logo"]["input"] = "html";
+        $form_fields["wc_custom_logo"]["html"] = $this->getCustomiseBtnHTML( $post->ID );
+      }
       return $form_fields;
     }
+
+    /*
+    * THIS METABOX APPEARS INSIDE VARIATIONS PRODUCT IN THE SINGLE PRODUCT PAGE
+    */
+    function addToVariableMetabox( $loop, $variation_data, $variation ){
+      $thumbnail_id = get_post_thumbnail_id( $variation );
+      if( $thumbnail_id ){
+        echo $this->getCustomiseBtnHTML( $thumbnail_id );
+      }
+    }
+
+
   }
 
   new WC_CUSTOM_LOGO_ADMIN;
