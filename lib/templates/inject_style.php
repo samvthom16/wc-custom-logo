8271975a0a36f6f38eabd08f0aa504902df5ff9f
Samuel Thomas <samvthom16@gmail.com>
2022-03-16 05:53:19 +0530

    Hide default logo placeholder in frontend

--- lib/templates/inject_style.php
+++ lib/templates/inject_style.php
@@ -4,6 +4,8 @@
     top     : <?php _e( $dimensions['wc_logo_top'] );?>%;
     left    : <?php _e( $dimensions['wc_logo_left'] );?>%;
     height  : <?php _e( $dimensions['wc_logo_width'] );?>%;
+    <?php if( is_admin() ):?>
     background-image: url( '<?php _e( wc_default_logo_placeholder( $dimensions['wc_logo_type'] ) );?>' );
+    <?php endif;?>
   }
 </style>
