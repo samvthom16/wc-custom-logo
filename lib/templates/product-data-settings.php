0ffb2796cfd9ec948be3f8315dfa5ae86b2ddbea
Samuel Thomas <samvthom16@gmail.com>
2022-10-15 11:49:27 +0530

    allow decimals in discount

--- lib/templates/product-data-settings.php
+++ lib/templates/product-data-settings.php
@@ -24,7 +24,7 @@
   <?php foreach( $discount_breaks as $discount_key ):?>
   <p class='form-field'>
     <label><?php _e( "Discount for $discount_key" );?></label>
-    <input type='number' class='short wc_input_price' name='wc_custom_discount_breaks[<?php echo $discount_key;?>]' value='<?php echo $discount_break_values[ $discount_key ];?>' />
+    <input type='number' class='short wc_input_price' step='0.01' name='wc_custom_discount_breaks[<?php echo $discount_key;?>]' value='<?php echo $discount_break_values[ $discount_key ];?>' />
   </p>
   <?php endforeach;?>
 </div>
